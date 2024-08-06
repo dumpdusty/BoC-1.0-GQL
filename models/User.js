7741b584f3e340bdd0718b5e22f9a7c642380e17
Fazil M <81851166+dumpdusty@users.noreply.github.com>
2024-08-06 16:34:41 -0700

    Server mongodb installation (#1)
    * add server and connect to mongoDB
    
    * add user model
    
    * add comment model
    
    * update index.js
    
    * add nodemon and express
    
    * add resolvers anf typedefs
    
    * add generateId.js
    
    * update User.js and Comments.js
    
    * add start script

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose')
+const {model, Schema} = require('mongoose')
+mongoose.Schema.Types.String.set('trim', true)
+
+
+const userSchema = new Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+        firstName: String,
+        lastName: String,
+        comments: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: `Comment`
+        }]
+    }
+)
+
+module.exports = model('User', userSchema)
